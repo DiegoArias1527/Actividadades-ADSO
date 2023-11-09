baa25159133740a370f3401d462c024be74c4ffe
DiegoArias1527 <diego.arias.torres15@gmail.com>
2023-11-08 22:27:28 -0500

    Actividad POST parte 1

--- controller/consignacion.php
+++ controller/consignacion.php
@@ -11,5 +11,20 @@
             $datos=$consignacion->get_consignacion();
             echo json_encode($datos);
         break;
+
+        case "GetId":
+        $datos=$consignacion->get_consignacion_x_id($body["id_Consignacion"]);
+        echo json_encode($datos);
+        break;
+
+        case "Insert":
+            $datos=$consignacion->insert_consignacion(
+                $body["num_Cuenta"],
+                $body["valor_Consignacion"],
+                $body["fecha_Consignacion"],
+                $body["id_Usuario"]
+            );
+            echo "Correcto";
+        break;
     }
 ?>
--- models/Consignacion.php
+++ models/Consignacion.php
@@ -1,13 +1,36 @@
 <?php
-    class Consignacion extends Conenctar{
-        public function get_consignacion(){
-            $conectar= parent::Conexion();
-            parent::set_names();
-            $sql="SELECT * FROM consignacion";
-            $sql=$conectar->prepare($sql);
-            $sql->execute();
-            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
-        }
-
+class Consignacion extends Conectar{
+    public function get_consignacion(){
+        $conectar = parent :: conexion();
+        parent:: set_names();
+        $sql="SELECT * FROM consignacion";
+        $sql=$conectar->prepare($sql);
+        $sql->execute();
+        return $resultado=$sql->fetchAll();
     }
+
+    public function get_consignacion_x_id($id_Consignacion){
+        $conectar= parent::Conexion();
+        parent::set_names();
+        $sql="SELECT * FROM consignacion WHERE id_Consignacion = ?";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $id_Consignacion);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function insert_consignacion($num_Cuenta,$valor_Consignacion,$fecha_Consignacion,$id_Usuario){
+        $conectar= parent::Conexion();
+        parent::set_names();
+        $sql="INSERT INTO consignacion(num_Cuenta,valor_Consignacion,fecha_Consignacion,id_Usuario) VALUES (?, ?, ?, ?);";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $num_Cuenta);
+        $sql->bindValue(2, $valor_Consignacion);
+        $sql->bindValue(3, $fecha_Consignacion);
+        $sql->bindValue(4, $id_Usuario);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+}
 ?>
